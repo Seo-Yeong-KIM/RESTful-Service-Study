7feb7086d6447e353ff6b5b8060409f3356262b1
Seo-Yeong-KIM <pepper2727@naver.com>
2022-11-18 11:56:19 +0900

    다국어 설정 추가
    메세지 properties 추가

--- restful/src/main/java/com/clrobur/restful/RestfulApplication.java
+++ restful/src/main/java/com/clrobur/restful/RestfulApplication.java
@@ -2,12 +2,25 @@ package com.clrobur.restful;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestfulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApplication.class, args);
+	}
+
+	// 다국어 설정을 위한 Bean 등록
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
 	}
 
 }
--- restful/src/main/java/com/clrobur/restful/hello/HelloController.java
+++ restful/src/main/java/com/clrobur/restful/hello/HelloController.java
@@ -1,11 +1,20 @@
 package com.clrobur.restful.hello;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    private MessageSource messageSource;
+
 
     @GetMapping("/hello")
     public String getHello() {
@@ -22,4 +31,12 @@ public class HelloController {
     public HelloBean beanHello(@PathVariable String name) {
         return new HelloBean(String.format("Hello, %s", name));
     }
+
+
+    // 다국어 설정
+    @GetMapping("/internationalized")
+    public String idInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
+
 }
