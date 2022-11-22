ed80627dd665d91238834ee4825f8c74da7f9234
Seo-Yeong-KIM <pepper2727@naver.com>
2022-11-22 14:51:17 +0900

    h2 db 연동
    db 연동하고 자동으로 user 테이블 생성하게 만듦

--- restful/src/main/java/com/clrobur/restful/config/SecurityConfig.java
+++ restful/src/main/java/com/clrobur/restful/config/SecurityConfig.java
@@ -3,10 +3,19 @@ package com.clrobur.restful.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // h2 DB 콘솔(/h2-console/**)에 대해서는 시큐리티 인증을 받지 않게 함
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
--- restful/src/main/java/com/clrobur/restful/user/User.java
+++ restful/src/main/java/com/clrobur/restful/user/User.java
@@ -9,6 +9,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -19,12 +22,15 @@ import java.util.Date;
 //@JsonIgnoreProperties(value={"password", "ssn"}) // JsonIgnore와 동일한 역할을 함
 //@JsonFilter("UserInfo") // 필터 사용하려면 이 주석 해제해야함
 @ApiModel(description = "사용자 상세 정보를 위한 도메인(info) 객체")
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min=2, message = "2글자 이상 입력해주세요")
-    @ApiModelProperty(notes = "사용자 이름을 입력하세요")
+    @ApiModelProperty(notes = "사용자 이름을 입력하세요") // api문서에서 해당 주석이 보이게 함
     private String name;
 
     @Past // 미래 데이터는 사용 불가. 과거 데이터만 사용 가능
